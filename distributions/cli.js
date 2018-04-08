5d690d0686e7eedee308c23f64436508e81b14cd
Rouven Weßling <rouven@contentful.com>
2018-04-08 14:05:32 +0200

    Upgrade babel to version 6

--- distributions/cli.js
+++ distributions/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 'use strict';
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 var _index = require('./index');
 
 var _index2 = _interopRequireDefault(_index);
 
-var reporter = (0, _index2['default'])();
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var reporter = (0, _index2.default)();
 
 process.stdin.pipe(reporter).pipe(process.stdout);
 
